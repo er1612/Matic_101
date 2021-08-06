406c51d8ef77579838aa7ddc939681212b3654a6
Revista ECyS - Ciencias, Sistemas & Tecnología <elmer.garcia90@outlook.com>
2021-08-06 01:17:25 -0600

    Commit de pagina
    Sitio de consumo agregado.

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
+/*app.get('/', (req, res) => {
   res.send('¡Hola Mundo! Desde Ubuntu AWS')
-})
+})*/
+
+app.get('/', function(request, response){
+  response.sendFile('pages/index.html', { root : __dirname});
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
